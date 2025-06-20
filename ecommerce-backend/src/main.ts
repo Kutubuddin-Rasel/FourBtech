e373e5b324bc005034e057446af77dbe8773f60c
Kutubuddin Juwel <145917927+Kutubuddin-Rasel@users.noreply.github.com>
2025-06-21 00:42:03 +0600

    Update main.ts

--- ecommerce-backend/src/main.ts
+++ ecommerce-backend/src/main.ts
@@ -1,3 +1,4 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -6,17 +7,20 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
   const origin =
     process.env.NODE_ENV === 'production'
-      ? process.env.FRONTEND_URL  
+      ? process.env.FRONTEND_URL!       
       : 'http://localhost:3000';
 
   app.enableCors({
     origin,
     credentials: true,
   });
+  const port = process.env.PORT
+    ? parseInt(process.env.PORT, 10)
+    : 3001;
 
-  const port = parseInt(process.env.PORT, 10) || 3001;
   await app.listen(port);
   console.log(`🚀 Listening on port ${port}`);
 }
