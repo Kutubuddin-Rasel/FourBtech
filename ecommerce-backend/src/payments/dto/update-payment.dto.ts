d6ad6a53b25838f9057644ce34edf49cf8790274
Kutubuddin Juwel <145917927+Kutubuddin-Rasel@users.noreply.github.com>
2025-06-29 01:01:09 +0600

    Delete ecommerce-backend directory

--- ecommerce-backend/src/payments/dto/update-payment.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsEnum, IsOptional } from 'class-validator';
-import { PaymentStatus } from '../entities/payment.entity';
- 
-export class UpdatePaymentDto {
-  @IsEnum(PaymentStatus)
-  @IsOptional()
-  status?: PaymentStatus;
-} 
